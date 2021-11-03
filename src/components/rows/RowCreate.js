cc30e5604746f07d4e90245e4280d9a43d42898b
Matt Krause <krause.matt@pm.me>
2021-11-02 21:48:07 -0400

    Created text inputs for plant and variety information along with input error checking before and at form submission

--- src/components/rows/RowCreate.js
+++ src/components/rows/RowCreate.js
@@ -1,30 +1,63 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form"
+import { Field, reduxForm } from "redux-form";
 
 class RowCreate extends React.Component {
-  renderInput({input, label}) {
-    console.log()
+
+  errorCheck = ({error, touched}) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    };
+  };
+
+  renderInput = ({input, label, meta}) => {
+    const fieldClass = `field ${meta.error && meta.touched ? "error" : ""}`
+
     return (
-      <div className="field">
+      <div className={fieldClass}>
         <label>{label} </label>
-        <input {...input} />
+        <input {...input} autoComplete="off"/>
+        {this.errorCheck(meta)}
       </div>
       
     );
   };
 
+  onSubmit(formValues) {
+    console.log(formValues)
+  };
+
 
   render() {
     return (
-      <form className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="plant" component={this.renderInput} label="Enter Plant" />
         <Field name="variety" component={this.renderInput} label="Enter Variety" />
+        <button className="ui button teal">Submit</button>
       </form>
     );
   };
-  }
+};
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.plant) {
+    errors.plant = "Please enter a plant";
+  };
+
+  if (!formValues.variety) {
+    errors.variety = "Please enter the variety";
+  };
+
+  return errors;
+};
   
 
 export default reduxForm({
-  form: "rowCreate"
+  form: "rowCreate",
+  validate: validate
 })(RowCreate);
