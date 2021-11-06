b4032c6e3e5fd315b6258c87a4c8534581b8a8f0
Matt Krause <krause.matt@pm.me>
2021-11-05 21:37:00 -0400

    Rendering all plant rows to home page

--- src/components/rows/RowList.js
+++ src/components/rows/RowList.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getRows } from "../../actions/index";
 
-const RowList = () => {
-  return (
-    <div>RowList</div>
-  );
+class RowList extends React.Component {
+
+  componentDidMount() {
+    this.props.getRows();
+  };
+
+  rowList() {
+    return this.props.rows.map((row) => {
+      return <div>{row.plant}</div>;
+    });
+  };
+  
+  render() {
+    return (
+    <div>{this.rowList()}</div>
+    );
+  };
+  
 };
 
-export default RowList;
+const mapStateToProps = (state) => {
+  return {rows: Object.values(state.rows)}
+};
+
+export default connect(mapStateToProps, {getRows})(RowList);
