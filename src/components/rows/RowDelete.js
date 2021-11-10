281f35e1e94ddfc4ec04961c63d56b162e9dee25
Matt Krause <krause.matt@pm.me>
2021-11-09 21:25:21 -0500

    Created modal to appear when user would like to delete a garden row

--- src/components/rows/RowDelete.js
+++ src/components/rows/RowDelete.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Modal from "../Modal";
 
 const RowDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+
   return (
-    <div>RowDelete</div>
+    <div>
+      RowDelete
+      <Modal 
+        title="Delete Garden Row"
+        content="Are you sure you'd like to delete this garden row?"
+        actions={actions}
+      /> 
+    </div>
   );
 };
 
