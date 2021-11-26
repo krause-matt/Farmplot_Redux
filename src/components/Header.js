972de5b729a7a8657efe1379eefbc6a8ecf02c50
Matt Krause <krause.matt@pm.me>
2021-11-26 10:00:50 -0500

    Styling collapsable top bar for app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
+import logo from "./images/logo.png";
+
+// const Header = () => {
+//   return (
+//     <div className="ui inverted segment">
+//       <div className="ui inverted secondary menu">
+//         <div className="active item">
+//           <Link to="/">Gardens</Link>
+//         </div>
+//         <div className="right item">
+//           <div className="item">
+//             <GoogleAuth />
+//           </div>
+//         </div>        
+//       </div>
+//     </div>
+//   );
+// };
 
 const Header = () => {
   return (
-    <div className="ui inverted segment">
-      <div className="ui inverted secondary menu">
-        <div className="active item">
-          <Link to="/">Gardens</Link>
-        </div>
-        <div className="right item">
-          <div className="item">
-            <GoogleAuth />
-          </div>
-        </div>        
+    <div className="ui stackable menu blue">
+      <div className="item">
+        <img src={logo} />
       </div>
-    </div>
+      <div className="item">
+      <Link to="/">Gardens</Link>
+      </div>
+      <div className="right item">
+        <GoogleAuth />
+      </div>
+    </div>  
   );
 };
 
